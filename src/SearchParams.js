2589bd83ff42b5760e11aeb40412cf7fb63474b5
Captain Storm <CaptainStorm21@users.noreply.github.com>
2020-09-14 00:12:59 -0700

    dropdown and calliing Animal API

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, Wa");
+  const [animal, setAnimal] = useState("dog");
   return (
     <div className="search-params">
       <form>
         <span>
-          <h2>
-            Your Location is: {location}
-          </h2>
+          <h2>Your Location is: {location}</h2>
         </span>
         <label htmlFor="location">
           location
@@ -20,6 +20,20 @@ const SearchParams = () => {
               setLocation(e.target.value);
             }}
           />
+          <label htmlFor="animal">
+            Animal
+            <select
+              id="animal"
+              value={animal}
+              onChange={(e) => setAnimal(e.target.value)}
+              onBlur={(e) => setAnimal(e.target.value)}
+            >
+              <option>All</option>
+              {ANIMALS.map((animal) => (
+                <option value={animal}>{animal}</option>
+              ))}
+            </select>
+          </label>
           <button>Submit</button>
         </label>
       </form>
