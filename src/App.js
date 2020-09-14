652623fe04f7c01d7ec0b9b066abf14e8389a32a
Captain Storm <CaptainStorm21@users.noreply.github.com>
2020-09-13 20:42:16 -0700

    Updated App.js JSX converion

--- src/App.js
+++ src/App.js
@@ -4,34 +4,45 @@ import Pet from "./Pet";
 
 //your code is going here
 const App = () => {
-  return React.createElement(
-    "div",
-    //attributes {}
-    {
-      id: "internal-div",
-    },
-    [
-      React.createElement("h1", {}, "Adopt Me!"),
-      React.createElement(Pet, {
-        key: 1,
-        name: "Lune",
-        animal: "Dog",
-        breed: "Labrador",
-      }),
-      React.createElement(Pet, {
-        key: 2,
-        name: "Alice",
-        animal: "Fox",
-        breed: "Lisao",
-      }),
-      React.createElement(Pet, {
-        key: 3,
-        name: "Max",
-        animal: "Dog",
-        breed: "English settler",
-      }),
-    ]
-  );
+//   return React.createElement(
+//     "div",
+//     //attributes {}
+//     {
+//       id: "internal-div",
+//     },
+//     [
+//       React.createElement("h1", {}, "Adopt Me!"),
+//       React.createElement(Pet, {
+//         key: 1,
+//         name: "Lune",
+//         animal: "Dog",
+//         breed: "Labrador",
+//       }),
+//       React.createElement(Pet, {
+//         key: 2,
+//         name: "Alice",
+//         animal: "Fox",
+//         breed: "Lisao",
+//       }),
+//       React.createElement(Pet, {
+//         key: 3,
+//         name: "Max",
+//         animal: "Dog",
+//         breed: "English settler",
+//       }),
+//     ]
+//   );
+
+return (
+    <div>
+        <Pet name = "Luna" animal="Dog" breed="Poodle"/>
+        <Pet name = "Alie" animal="Cat" breed="Seamese"/>
+        <Pet name = "Alpha" animal="Eagle" breed="Bald Eagle"/>
+
+    </div>
+)
+
+
 };
 
 render(React.createElement(App), document.getElementById("root"));
